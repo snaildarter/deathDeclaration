85da31d3dc75b96f2bd63c36bc97f15288aa85ab
lilinshan <lilinshan@globalegrow.com>
2019-02-26 18:22:27 +0800

    初始化项目

---
+++ server.js
@@ -0,0 +1,36 @@
+let http = require('http'),
+    url = require('url'),
+    superagent = require('superagent'),
+    cheerio = require('cheerio'),
+    async = require('async'),
+    eventproxy = require('eventproxy');
+let ep = new eventproxy(),
+    urlsArray = [],
+    pageUrls = [],
+    pageNum = 200;
+
+const dataArr = [];
+const prefixRrl = 'https://www.tdcj.texas.gov/death_row/';
+
+superagent.get('https://www.tdcj.texas.gov/death_row/dr_executed_offenders.html')
+    .end(function(err, pres){
+        if (err) {
+            return throw Error(err);
+        } 
+        let $ = cheerio.load(pres.text);
+        $('.tdcj_table.indent tr').each(function(i){
+            let td = $(this).eq(0).find('td');
+        
+            dataArr.push({
+                execution: td.eq(0).text(),
+                userInfoUrl:  prefixRrl + td.eq(1).find('a').attr('href'),
+                lastStatement: prefixRrl + td.eq(2).find('a').attr('href'),
+                name: td.eq(3).text() + ' ' + td.eq(4).text(),
+                age: td.eq(6).text(),
+                date: td.eq(7).text(),
+            });
+        });
+        console.log(dataArr);
+    });
+
+
