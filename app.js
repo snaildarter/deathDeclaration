59a1b920b7c4c598e02af6d9e0bf7d236f97a2b8
snail <1107554021@qq.com>
2019-03-02 11:10:11 +0800

    更新部分代码--解决部分数据没有爬取成

--- app.js
+++ app.js
@@ -21,21 +21,23 @@ superagent.get(deathRowInfoUrl).end(function (err, pres) {
     $('.tdcj_table.indent tr').each(function(){
         let td = $(this).eq(0).find('td');
 
-        if (!td.eq(2).find('a').attr('href')) {
-            return;
+        if (td.eq(2).find('a').attr('href')) {
+            if (td.eq(2).find('a').attr('href').includes('dr_info/coblebillielast.html') ) {
+                return;
+            }
+            urlsArray.push(protocol + prefixRrl + td.eq(2).find('a').attr('href').replace(/\/death_row\//, ''));
+            dataArr.push({
+                execution: td.eq(0).text(),
+                userInfoUrl:  protocol + prefixRrl + td.eq(1).find('a').attr('href').replace(/\/death_row\//, ''),
+                lastStatement: protocol + prefixRrl + td.eq(2).find('a').attr('href').replace(/\/death_row\//, ''),
+                name: td.eq(3).text() + ' ' + td.eq(4).text(),
+                age: td.eq(6).text(),
+                date: td.eq(7).text(),
+            });
         }
-        urlsArray.push(protocol + prefixRrl + td.eq(2).find('a').attr('href').replace(/\/death_row\//, ''));
-        dataArr.push({
-            execution: td.eq(0).text(),
-            userInfoUrl:  protocol + prefixRrl + td.eq(1).find('a').attr('href').replace(/\/death_row\//, ''),
-            lastStatement: protocol + prefixRrl + td.eq(2).find('a').attr('href').replace(/\/death_row\//, ''),
-            name: td.eq(3).text() + ' ' + td.eq(4).text(),
-            age: td.eq(6).text(),
-            date: td.eq(7).text(),
-        });
     });
     
-    getInfo(urlsArray);
+    getInfo();
 });
 
 // 获取信息
@@ -43,25 +45,23 @@ function getInfo() {
     let reptileMove = function (url, callback) {
         superagent.get(url).end(function (err, sres) {
             if (err) {
-                log(err);
+                log(JSON.stringify(err, null, 4));
                 return;
             }
             let i = urlsArray.indexOf(url);
             dataArr[i]['lastStatement'] = cheerio.load(sres.text)('#content_right').text().split('Last Statement:')[1];
+            log('获取成功', i);
             callback(null, i);
-            log('获得成功', url, i);
         });
     };
     // 使用async控制异步抓取  mapLimit(arr, limit, iterator, [callback]) 异步回调
     //  网路不好的时候limit最好写小一点，不然有问题的
-    async.mapLimit(urlsArray, 3, function (url, callback) {
+    async.mapLimit(urlsArray, 8, function (url, callback) {
         reptileMove(url, callback);
     }, function (err, result) {
         if (err) log(err);
         log('爬取完成');
-        setTimeout(function(){
-            fs.writeFileSync('./data.js', JSON.stringify(dataArr, null, 4) , 'utf-8');
-            log('写入完成');
-        }, 0);
+        fs.writeFileSync('./data.js', JSON.stringify(dataArr, null, 4) , 'utf-8');
+        log('写入完成');
     });
 }
